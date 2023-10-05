5d36a3b8a03aef6836216ba0774debf5ec19f52a
maulikam <mdave.5191@gmail.com>
2023-10-05 15:57:02 +0530

    slices in go lang

--- main.go
+++ main.go
@@ -4,4 +4,27 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+	var i int = 10
+	i /= 10
+
+	var xx int = 10
+	var y float64 = 30.2
+	var z float64 = float64(xx) + y
+	var d int = xx + int(y)
+
+	fmt.Println(z)
+	fmt.Println(d)
+
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 10)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 20)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 30)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 40)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 50)
+	fmt.Println(x, len(x), cap(x))
 }
